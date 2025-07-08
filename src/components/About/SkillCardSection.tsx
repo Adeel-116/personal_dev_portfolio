acf6bb7cb8fa615a4e7abcb8b2d702cda8868fd6
Adeel-116 <adeel8128377@gmail.com>
2025-07-08 08:16:32 -0700

    fix errorss

--- src/components/About/SkillCardSection.tsx
+++ src/components/About/SkillCardSection.tsx
@@ -1,13 +1,13 @@
-import SkillCard from "./SkillCard";
+// import SkillCard from "./SkillCard";
 
-const SkillCardSection = () => {
-  return (
-    <div className="w-full flex lg:flex-row justify-center  items-center xl:gap-5 lg:gap-3 gap-y-5 flex-col">
-      <SkillCard children="lg:flex-1 w-[95%] " />
-      <SkillCard children="lg:flex-1 w-[95%] " />
-      <SkillCard children="lg:flex-1 w-[95%] " />
-    </div>
-  );
-};
+// const SkillCardSection = () => {
+//   return (
+//     <div className="w-full flex lg:flex-row justify-center  items-center xl:gap-5 lg:gap-3 gap-y-5 flex-col">
+//       <SkillCard children="lg:flex-1 w-[95%] " />
+//       <SkillCard children="lg:flex-1 w-[95%] " />
+//       <SkillCard children="lg:flex-1 w-[95%] " />
+//     </div>
+//   );
+// };
 
-export default SkillCardSection;
+// export default SkillCardSection;
