2b5247d4f4ea62da5998b04fc9d3f99d8b9413f3
Adeel-116 <adeel8128377@gmail.com>
2025-07-16 07:19:57 -0700

    Deloy error fix

--- src/components/ImageComponent.tsx
+++ src/components/ImageComponent.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import HeroSection from './HeroSection/HeroSection'; 
 import MouseCircle from './MouseCircle'; 
 import AboutUS from './About/AboutUS'; 
@@ -9,71 +10,81 @@ import Testimonials from './testimonial/Testimonials';
 import backGroundImage from '../assets/backGroundImage.jpg'; 
 import Portfolio from './portfolio-section/PortfolioSection'; 
 import Footer from './Footer/Footer'; 
-import {Element} from "react-scroll"
+import { Element } from "react-scroll"
 import Header from './Header/Header';
 
-function ImageComponent() { 
-  return ( 
-    <div 
-      className="w-full min-h-screen flex flex-col items-center" 
-      style={{ backgroundImage: `url(${backGroundImage})`, width: "100%", backgroundSize: "cover", minHeight: '100vh' }} 
-    > 
-      <div className='w-full'> 
-        <Header /> 
-      </div> 
+function ImageComponent() {
+  const [imageLoaded, setImageLoaded] = useState(false);
 
-      {/* Hero Section */}
+  useEffect(() => {
+    const img = new Image();
+    img.src = backGroundImage;
+    img.onload = () => setImageLoaded(true);
+  }, []);
+
+  if (!imageLoaded) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center bg-black text-white text-lg">
+        Loading background...
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className="w-full min-h-screen flex flex-col items-center"
+      style={{
+        backgroundImage: `url(${backGroundImage})`,
+        width: "100%",
+        backgroundSize: "cover",
+        minHeight: '100vh'
+      }}
+    >
+      <div className='w-full'>
+        <Header />
+      </div>
+
       <Element name="home" className='2xl:w-[75%] xl:w-[85%] sm:w-[90%] w-[95%] justify-center items-center'>
         <HeroSection />
-      </Element>  
+      </Element>
 
-      {/* About Section */}
       <Element name="about" className='2xl:w-[75%] xl:w-[85%] sm:w-[90%] w-[95%] justify-center items-center'>
         <AboutUS />
       </Element>
 
-      {/* Skills */}
       <Element name="skills" className='2xl:w-[75%] xl:w-[85%] sm:w-[90%] w-[95%]'>
         <SkillSection />
-      </Element>  
+      </Element>
 
-      {/* Portfolio */}
       <Element name="portfolio" className='2xl:w-[75%] xl:w-[85%] sm:w-[90%] justify-center items-center'>
         <Portfolio />
       </Element>
 
-      {/* Services */}
       <Element name="services" className='2xl:w-[75%] xl:w-[85%] sm:w-[90%] w-[95%]'>
-        <ServiceSection />  
+        <ServiceSection />
       </Element>
 
-      {/* Testimonials */}
       <Element name="testimonials" className='2xl:w-[75%] xl:w-[85%] sm:w-[90%] w-[95%]'>
         <Testimonials />
       </Element>
 
-
-      {/* Contact */}
       <Element name="contact" className='2xl:w-[75%] xl:w-[85%] sm:w-[90%] w-[95%]'>
         <Contact />
-      </Element>  
+      </Element>
 
-      {/* Newsletter */}
-      <div className='2xl:w-[75%] xl:w-[85%] sm:w-[90%] flex justify-center py-10 w-[95%]'> 
-        <NewsLetter /> 
-      </div>  
+      <div className='2xl:w-[75%] xl:w-[85%] sm:w-[90%] flex justify-center py-10 w-[95%]'>
+        <NewsLetter />
+      </div>
 
-      {/* Footer */}
-      <div className='w-full'> 
-        <Footer /> 
-      </div>  
+      <div className='w-full'>
+        <Footer />
+      </div>
 
-      {/* Mouse Circle */}
-      <div className="absolute top-0 left-0 w-full h-full pointer-events-none z-50"> 
-        <MouseCircle /> 
-      </div>  
-    </div> 
-  ) 
-} 
+      <div className="absolute top-0 left-0 w-full h-full pointer-events-none z-50">
+        <MouseCircle />
+      </div>
+    </div>
+  );
+}
 
 export default ImageComponent;
